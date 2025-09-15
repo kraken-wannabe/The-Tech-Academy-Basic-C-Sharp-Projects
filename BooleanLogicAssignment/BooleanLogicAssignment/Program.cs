13667c797c15c7bab8f07dc371e3091915b55e10
kraken-wannabe <wireliness@gmail.com>
2025-09-15 14:16:46 -0400

    Branching

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace BooleanLogicAssignment
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Boolean Logic Assignment");
+        //Ask the user to enter their age.
+        Console.Write("\nWhat is your age? ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // Ask if the user has ever had a DUI.
+        Console.Write("\nHave you ever had a DUI? (True/False) ");
+        bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+        // Ask how many speeding tickets the user has.
+        Console.Write("\nHow many speeding tickets do you have? ");
+        int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            // Qualification rules:
+            //1. Must be over 15 years old.
+            //2. Must not have any DUIs.
+            //3. Must have 3 or fewer speeding tickets.
+            bool isQualified = (age > 15) && (!hasDUI) && (speedingTickets <= 3);
+
+            // Print result
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(isQualified);
+
+            // Keep console window open until user presses a key
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+
+
+namespace Branching
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favNumber = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNumber == 12 ? "You have an awesome favorite number!" : "You do not have an awesome favorite number.";
+            Console.WriteLine(result);
+
+            //int currentTemperature = 72;
+            //int roomTemperature = 71;
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hello! What is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi " + name + "! What is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("It is cooler than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhh... something went wrong.");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
