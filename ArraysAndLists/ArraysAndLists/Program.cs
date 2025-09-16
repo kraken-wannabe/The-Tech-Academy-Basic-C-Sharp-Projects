edc0a25833aacf7849286ade18f6050e33144e61
kraken-wannabe <wireliness@gmail.com>
2025-09-16 13:37:08 -0400

    Adding Console app array Assignment

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("World");
+        intList.Remove("World");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+
+        ////Option 1 array declaration
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+        ////Option 2 array declaration
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+        ////Option 3 array declaration
+        //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+        ////target an array to change number
+        //numArray2[3] = 250;
+
+        //Console.WriteLine(numArray2[3]);
+        //Console.ReadLine();
+    }
+}
---
+++ Console App Array Assignment`/Console App Array Assignment`/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Console_App_Array_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of strings
+            string[] nameArray = { "Alice", "Bob", "Charlie", "Mike", "Mira", "Ken", "Dom" };
+            // ask user to input 0-6
+            Console.WriteLine("Please enter a number between 0 and 6:");
+            int stringSelect = Convert.ToInt32(Console.ReadLine());
+            bool validStringSelect = false;
+
+            while (!validStringSelect)
+            {
+                if (stringSelect >= 0 && stringSelect <= 6)
+                {
+                    validStringSelect = true;
+                    Console.WriteLine("You selected: " + nameArray[stringSelect]);
+                }
+                else
+                {
+                    //if false , ask user to input 0-6 again
+                    Console.WriteLine("Invalid input. Please enter a number between 0 and 6:");
+                    stringSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            // Array of Lists
+            List<string> colorListArray = new List<string>()
+            {
+                "red",
+                "blue",
+                "green",
+                "yellow",
+                "orange",
+                "purple",
+                "black",
+                "white",
+                "gray"
+            };
+            // ask user to input 0-8
+            Console.WriteLine("Please enter a number between 0 and 8:");
+            int colorSelect = Convert.ToInt32(Console.ReadLine());
+            bool validColorSelect = false;
+            while (!validColorSelect)
+            {
+                if (colorSelect >= 0 && colorSelect <= 8)
+                {
+                    validColorSelect = true;
+                    Console.WriteLine("You selected: " + colorListArray[colorSelect]);
+                }
+                else
+                {
+                    //if false , ask user to input 0-8 again
+                    Console.WriteLine("Invalid input. Please enter a number between 0 and 8:");
+                    colorSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
