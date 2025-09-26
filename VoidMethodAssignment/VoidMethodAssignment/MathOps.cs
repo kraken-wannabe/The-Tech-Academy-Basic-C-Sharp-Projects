b830b65721a27d1a81e74f6e7cbf9f741ec5606e
kraken-wannabe <wireliness@gmail.com>
2025-09-26 02:30:12 -0400

    Adding Void Method Assignment

---
+++ VoidMethodAssignment/VoidMethodAssignment/MathOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    //
+    public class MathOps
+    {
+        private int lastComputed; //stores result of math on first int
+
+        //takes two int. Does math on first and prints second.
+        public void Process(int first, int second)
+        {
+            lastComputed = first * 2; //math on first
+            Console.WriteLine(second); //show second
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            MathOps ops = new MathOps();
+
+            //call the method class passing two numbers
+            ops.Process(5, 42);    
+
+            //call the method again specifying parameters by name
+            ops.Process(second: 99, first: 3); 
+
+            //ENd program
+            Console.WriteLine("\nPress Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
