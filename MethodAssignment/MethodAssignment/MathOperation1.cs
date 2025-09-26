9769715d092bf6622f8929465b82ebbd29a4a8c0
kraken-wannabe <wireliness@gmail.com>
2025-09-26 01:00:36 -0400

    Adding MethodAsssignment

---
+++ MethodAssignment/MethodAssignment/MathOperation1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathOperation1
+    {
+        //Create a method that takes two integers as parameters
+        //Make one parameter optional by providing a default value
+        public int MathOp(int i, int j= 1)
+        {
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate MathOperation1 class
+            MathOperation1 mathOp = new MathOperation1();
+            //ask user to proviude parameter
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //Ask user to provide a second number or press enter
+            Console.WriteLine("Enter a second number or just press enter: ");
+            try
+            {
+                //Call method with 2 parameters if both parameters are provided
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathOp.MathOp(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
+             }
+            catch
+            {
+                //Call methoiod with one parameter if only one is provided
+                int results = mathOp.MathOp(num1);
+                Console.WriteLine(num1 + " x Default 1 = " + results);
+            }
+            //End Program
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
