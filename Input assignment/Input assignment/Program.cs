d1116a17938d57452451ec60eb0a7c23ed0dda59
kraken-wannabe <wireliness@gmail.com>
2025-10-14 08:51:15 -0400

    adding input assignment folder

---
+++ Input assignment/Input assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+
+        // (Optional) echo to the console, similar to your "Console.WriteLine(card);"
+        Console.WriteLine(input);
+
+        // Write the number to a text file
+        string path = @"C:\Users\kraken\Documents\logs\log.txt";
+
+        //append this file path
+        using (StreamWriter file = new StreamWriter(path, true))
+        {
+            file.WriteLine(input);
+        }
+
+        // 3) Read the file and print back to user
+        string contents = File.ReadAllText(path);
+        Console.WriteLine("\nFile contents:");
+        Console.WriteLine(contents);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadLine();
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,21 +10,27 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-
-
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name:");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player; // operator overload
+                player.IsActivelyPlaying = true;
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player; // operator overload
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.ReadLine();
         }
-     
     }
 }
-
-
-
