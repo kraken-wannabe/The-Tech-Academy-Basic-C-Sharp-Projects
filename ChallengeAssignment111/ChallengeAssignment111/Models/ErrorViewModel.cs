347154a5fa24479287c0b1b088b199d5eb093e5b
kraken-wannabe <wireliness@gmail.com>
2025-10-30 13:50:58 -0400

    adding ChallengeAssignment111

---
+++ ChallengeAssignment111/ChallengeAssignment111/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChallengeAssignment111.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
