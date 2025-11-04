733d20137b44ca7b62c5c9e98adbace3942ea86c
kraken-wannabe <wireliness@gmail.com>
2025-11-03 23:21:44 -0500

    Adding CodeFirstStudent  folder for the final assignment

---
+++ CodeFirstStudent/CodeFirstStudent/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //open the database (creates it the first time)
+            using (var db = new SchoolContext())
+            {
+                // make one student
+                var s = new Student
+                {
+                    FirstName = "Juan",
+                    LastName = "Dela Cruz"
+                };
+
+
+                db.Students.Add(s); //stage the insert
+                db.SaveChanges(); //write to the databse
+
+                Console.WriteLine("Added 1 student.");
+            }
+
+            Console.WriteLine("Done. Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeFirstStudent/CodeFirstStudent/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstStudent
+{
+    public class SchoolContext : DbContext
+    {
+        
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+       //this becomes a table called Students
+        public DbSet<Student> Students { get; set; }
+    }
+}
